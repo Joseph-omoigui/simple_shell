c15a0463b7621e37350cf71bfec0a1439d40dde3
Joseph omoigui <122811283+Joseph-omoigui@users.noreply.github.com>
2023-05-23 10:22:11 +0100

    Add files via upload

---
+++ helper_1.c
@@ -0,0 +1,57 @@
+#define SHELL_H
+#include "shell.h"
+/**
+ * free_recurrent_data - free fields needed for each loop
+ * @data: the struct of the program's data
+ * Return: Nothing
+ */
+void free_recurrent_data(data_of_program *data)
+{
+	if (data->tokens)
+		free_array_of_pointers(data->tokens);
+	if (data->input_line)
+		free(data->input_line);
+	if (data->command_name)
+		free(data->command_name);
+
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->tokens = NULL;
+}
+
+/**
+ * free_all_data - always free all field of data
+ * @data: struct of program's data
+ * Return: Nothing
+ */
+void free_all_data(data_of_program *data)
+{
+	if (data->file_descriptor != 0)
+	{
+		if (close(data->file_descriptor))
+			perror(data->program_name);
+	}
+	free_recurrent_data(data);
+	free_array_of_pointers(data->env);
+	free_array_of_pointers(data->alias_list);
+}
+
+/**
+ * free_array_of_pointers - frees each pointer of an array of pointers and the
+ * array too
+ * @array: array of pointers
+ * Return: nothing
+ */
+void free_array_of_pointers(char **array)
+{
+	int a;
+
+	if (array != NULL)
+	{
+		for (a = 0; array[a]; a++)
+			free(array[a]);
+
+		free(array);
+		array = NULL;
+	}
+}
