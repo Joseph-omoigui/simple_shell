610d548ef2df9b75c43fb84f6076b9e45faf8c2b
Joseph-omoigui <josephomoigui100@gmail.com>
2023-05-23 08:58:39 -0700

    team work

--- str_token.c
+++ str_token.c
@@ -1,3 +1,4 @@
+#define SHELL_H
 #include "shell.h"
 /**
  * _strtok - it separates strings with delimiters
@@ -7,36 +8,35 @@
 */
 char *_strtok(char *line, char *delim)
 {
-	int b;
-	static char *str;
-	char *copystr;
-
-	if (line != NULL)
-		str = line;
-	for (; *str != '\0'; str++)
-	{
-		for (b = 0; delim[b] != '\0'; b++)
-		{
-			if (*str == delim[b])
-			break;
-		}
-		if (delim[b] == '\0')
-			break;
-	}
-	copystr = str;
-	if (*copystr == '\0')
-		return (NULL);
-	for (; *str != '\0'; str++)
-	{
-		for (b = 0; delim[b] != '\0'; b++)
-		{
-			if (*str == delim[b])
-			{
-				*str = '\0';
-				str++;
-				return (copystr);
-			}
-		}
-	}
-	return (copystr);
+int b;
+static char *str;
+char *copystr;
+if (line != NULL)
+str = line;
+for (; *str != '\0'; str++)
+{
+for (b = 0; delim[b] != '\0'; b++)
+{
+if (*str == delim[b])
+break;
+}
+if (delim[b] == '\0')
+break;
+}
+copystr = str;
+if (*copystr == '\0')
+return (NULL);
+for (; *str != '\0'; str++)
+{
+for (b = 0; delim[b] != '\0'; b++)
+{
+if (*str == delim[b])
+{
+*str = '\0';
+str++;
+return (copystr);
+}
+}
+}
+return (copystr);
 }
